9470ae77d67b0130ab946ddf148af32f43c62767
fbranog <frank@workspex.de>
2025-08-26 12:03:49 +0530

    plugin v0.1.0

--- router.py
+++ router.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 
 router = APIRouter(prefix='/faastapi_plugin_han_cock')
 
-@router.get('/')
+@router.get('/status')
 async def root():
     return {'message': 'Hello from faastapi_plugin_han_cock!'}
