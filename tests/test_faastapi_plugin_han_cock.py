9030ba8c4c104dff41751ae47c64370f931eab1b
fbranog <frank@workspex.de>
2025-08-27 13:03:05 +0530

    commented out test file in tests dir

--- tests/test_faastapi_plugin_han_cock.py
+++ tests/test_faastapi_plugin_han_cock.py
@@ -1,12 +1,12 @@
-from fastapi import FastAPI
-from fastapi.testclient import TestClient
-from faastapi_plugin_han_cock.router import router
+# from fastapi import FastAPI
+# from fastapi.testclient import TestClient
+# from faastapi_plugin_han_cock.router import router
 
-app = FastAPI()
-app.include_router(router)
-client = TestClient(app)
+# app = FastAPI()
+# app.include_router(router)
+# client = TestClient(app)
 
-def test_root():
-    resp = client.get('/faastapi_plugin_han_cock/')
-    assert resp.status_code == 200
-    assert resp.json() == {'message': 'Hello from faastapi_plugin_han_cock!'}
+# def test_root():
+#     resp = client.get('/faastapi_plugin_han_cock/')
+#     assert resp.status_code == 200
+#     assert resp.json() == {'message': 'Hello from faastapi_plugin_han_cock!'}
